1f22fa67d2d77deb0789c76e5d7e079b8d6b3f1b
zglawless <zakglawless@gmail.com>
2023-06-15 19:12:48 -0500

    finalized styling and functions

---
+++ action.php
@@ -0,0 +1,58 @@
+<?php
+if (isset($_POST['Email'])) {
+
+    // EDIT THE FOLLOWING TWO LINES:
+    $email_to = "zakglawless@gmail.com";
+    $email_subject = "Website Contact Form";
+
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $message = $_POST['subject']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
+
+    Thanks for getting in touch. I'll get back to you soon.
+
+<?php
+}
+?>
